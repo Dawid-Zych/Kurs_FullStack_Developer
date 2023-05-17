c77e164b5b5121a990577ea7464a6858374e815a
Dawid Zych <dawid.aliens1989@gmail.com>
2023-05-17 21:38:00 +0200

    simple server

---
+++ Node Js/simple_server/server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+    console.log('Request');
+	res.writeHead(200, { 'Content-type': 'text/html' });
+	res.end('Hello World!!');
+}).listen(8080);""
