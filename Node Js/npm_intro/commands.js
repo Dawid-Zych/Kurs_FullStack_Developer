31494390b439e2f1f696d217007a56f15e71c743
Dawid Zych <dawid.aliens1989@gmail.com>
2023-05-17 21:47:25 +0200

    npm intro

---
+++ Node Js/npm_intro/commands.js
@@ -0,0 +1,16 @@
+/*
+
+    npm init   - stworzenie nowego projektu
+    npm init -y  - stworzenie nowego projektu z domyślnymi wartościami
+
+    npm install lodash   - instalacja paczki do projektu
+    npm install underscore -D   - instalacja paczki do developmentu
+    npm install axios -g   - instalacja paczki globalnie dla
+                             wszystkich projektów
+
+    npm list   - listowanie paczek projektu
+    npm list -g   - listowanie globalnych paczek 
+
+    npm uninstall -g axios    - odinstalowanie globalnej paczki
+    npm uninstall -g underscore    - odinstalowanie paczki globalnie
+*/
