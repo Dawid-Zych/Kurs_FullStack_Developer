15a904c78ce741c723e6985b71fb3f8e4504ad73
Dawid Zych <dawid.aliens1989@gmail.com>
2023-05-25 19:49:14 +0200

    poprawka util z mymetype

--- projects/quotes_app/util/staticServer.js
+++ projects/quotes_app/util/staticServer.js
@@ -2,42 +2,48 @@ const fs = require('fs');
 const path = require('path');
 
 const mimeTypes = {
-	'.html': 'text/html',
-	'.js': 'text/javascript',
-	'.json': 'application/json',
-	'.css': 'text/css',
-	'.jpg': 'image/jpeg',
-	'.png': 'image/png',
+	'.html': 'text/html', // Typ MIME dla plików HTML
+	'.js': 'text/javascript', // Typ MIME dla plików JavaScript
+	'.json': 'application/json', // Typ MIME dla plików JSON
+	'.css': 'text/css', // Typ MIME dla plików CSS
+	'.jpg': 'image/jpeg', // Typ MIME dla plików JPEG
+	'.png': 'image/png', // Typ MIME dla plików PNG
 };
 
-// reg: /app.js
 function serveStaticFile(req, res) {
-	const baseURL = req.protocol + '://' + req.headers.host + '/';
-	const parsedURL = new URL(req.url, baseURL);
+	const baseURL = req.protocol + '://' + req.headers.host + '/'; // Bazowy URL żądania
+	const parsedURL = new URL(req.url, baseURL); // Parsowanie URL-a
 
-	let pathSanitize = path.normalize(parsedURL.pathname);
+	let pathSanitize = path.normalize(parsedURL.pathname); // Normalizacja ścieżki
 
-	let pathname = path.join(__dirname, '..', 'static', pathSanitize);
+	let pathname = path.join(__dirname, '..', 'static', pathSanitize); // Pełna ścieżka pliku
 
-	if (fs.existsSync(pathname)) {
-		if (fs.statSync(pathname).isDirectory()) {
-			pathname += '/index.html';
-		}
-
-		fs.readFile(pathname, function (err, data) {
-			if (err) {
-				res.statusCode = 500;
-				res.end('File not found: ' + err);
-			} else {
-				const extension = path.parse(pathname).ext;
-				res.setHeader('Content-Type', mimeTypes[extension]);
-				res.end(data);
+	fs.access(pathname, fs.constants.F_OK, err => {
+		if (err) {
+			// Plik nie istnieje - zwróć kod błędu 404 i komunikat
+			res.statusCode = 404;
+			res.end('File not found');
+		} else {
+			if (fs.statSync(pathname).isDirectory()) {
+				// Jeśli ścieżka jest katalogiem, dodaj '/index.html' na końcu
+				pathname += '/index.html';
 			}
-		});
-	} else {
-		res.statusCode = 404;
-		res.end('File not found');
-	}
+
+			fs.readFile(pathname, function (err, data) {
+				if (err) {
+					// Błąd odczytu pliku - zwróć kod błędu 500 i komunikat
+					res.statusCode = 500;
+					res.end('File not found: ' + err);
+				} else {
+					const extension = path.parse(pathname).ext; // Rozszerzenie pliku
+					const contentType = mimeTypes[extension] || 'application/octet-stream'; // Typ MIME pliku lub domyślny typ MIME
+
+					res.setHeader('Content-Type', contentType); // Ustawienie nagłówka 'Content-Type' w odpowiedzi
+					res.end(data); // Wysłanie danych pliku jako odpowiedź
+				}
+			});
+		}
+	});
 }
 
 module.exports = {
