71577f742af4277ad7a07a98fd2d660977f6b674
Dawid Zych <dawid.aliens1989@gmail.com>
2023-06-02 14:57:58 +0200

    laczenie z mongo i funkcje

---
+++ projects/mongoose_tasks/model/task.model.js
@@ -0,0 +1,81 @@
+import mongoose from 'mongoose';
+const url = 'mongodb://127.0.0.1:27017/mongoproject';
+
+mongoose
+	.connect(url)
+	.then(() => console.log('Pomyślnie połączono z bazą danych'))
+	.catch(err => {
+		console.error(err, '\n Błąd połączenia z bazą danych');
+	});
+
+const taskSchema = new mongoose.Schema({
+	_id: mongoose.Schema.Types.ObjectId,
+	title: {
+		type: String,
+		required: true,
+		trim: true,
+		minLength: 1,
+		maxLength: 256,
+	},
+	description: {
+		type: String,
+		required: false,
+		trim: true,
+		minLength: 1,
+		maxLength: 10240,
+	},
+	status: {
+		type: String,
+		required: true,
+		enum: ['Not started', 'In progress', 'On hold', 'Completed'],
+		default: 'Not started',
+	},
+	created: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+function makeTask(title, description) {
+	return new Task({
+		_id: new mongoose.Types.ObjectId(),
+		title: title,
+		description: description,
+	});
+}
+
+const tasksArr = [
+	makeTask('Task #1', 'Something to do #1'),
+	makeTask('Task #2', 'Something to do #2'),
+	makeTask('Task #3', 'Something to do #3'),
+	makeTask('Task #4', 'Something to do #4'),
+	makeTask('Task #5', 'Something to do #5'),
+];
+await Task.deleteMany();
+try {
+	const tasksDb = await Task.find();
+	console.log('Num tasks in db:', tasksDb.length);
+	if (tasksDb.length === 0) {
+		const newTask = await Task.insertMany(tasksArr);
+		if (newTask.length > 0) {
+			console.log(`Pobraliśmy nowe zadania w liczbie ${newTask.length}!`);
+		} else {
+			console.log('\nNie udało się pobrać nowych zadań!');
+		}
+	}
+} catch (error) {
+	console.log('Error: ', error.message);
+}
+
+export async function getAll() {
+	return await Task.find();
+}
+
+export async function getByID(_id) {
+	return await Task.findById({ _id });
+}
+export async function deleteByID(_id) {
+	return await Task.findByIdAndDelete({ _id });
+}
