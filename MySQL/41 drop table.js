8e22a112aa3cce74cd5bd5b11a6e9482640e94d7
Dawid Zych <dawid.aliens1989@gmail.com>
2023-06-11 18:34:38 +0200

    usuwanie tabeli

---
+++ MySQL/41 drop table.js
@@ -0,0 +1,27 @@
+
+import mysql from "mysql2/promise";
+
+const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test_db"
+});
+
+await connection.connect();
+
+
+async function dropTable(name) {
+    const sql = "DROP TABLE ??";
+    
+    await connection.query(sql, [name])
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+await dropTable("customers");
+console.log("Table deleted");
+
+
+
