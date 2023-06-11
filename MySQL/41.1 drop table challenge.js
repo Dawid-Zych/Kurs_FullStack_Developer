237be6fa4ef5563e25c377be5e2c89c5252b0a43
Dawid Zych <dawid.aliens1989@gmail.com>
2023-06-11 18:34:49 +0200

    usuwanie tabeli zadanie

---
+++ MySQL/41.1 drop table challenge.js
@@ -0,0 +1,30 @@
+/*  
+    Zadanie
+    1. Skasuj tabele smartphones z wykorzystaniem
+       funkcji dropTable(name)
+*/
+import mysql from "mysql2/promise"; 
+const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test"
+});
+
+await connection.connect();
+
+
+async function dropTable(name) {
+    const sql = "DROP TABLE ??";
+
+    await connection.query(sql, [name])
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+await dropTable("smartphones");
+
+await connection.close();
+
+
