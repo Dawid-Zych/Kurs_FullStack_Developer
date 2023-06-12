38f43ee2479da17ef81af9216474e71a23be4e53
Dawid Zych <dawid.aliens1989@gmail.com>
2023-06-12 20:45:07 +0200

    łaczenie kolumn concat_ws zadanie

---
+++ MySQL/43.1 concat challenge.js
@@ -0,0 +1,26 @@
+/*  
+    Zadanie:
+    1. Napisz funkcję która połączy id, name, surname, bio
+       użytkownika z CONCAT_WS(), wywołaj ją i pokaż wynik 
+       w konsoli.
+*/
+import mysql from "mysql2/promise";
+const connection = await mysql.createConnection({ 
+    host: "localhost", 
+    user: "root", 
+    password: "", 
+    database: "test"
+});
+
+await connection.connect(); 
+ 
+
+async function getUserData() {
+    const sql = "SELECT id, CONCAT_WS(', ', id, name, surname, bio) as 'userData' FROM users ";
+    const [rows] = await connection.query(sql);
+    return rows;
+}
+
+console.log( await getUserData() );
+
+await connection.close();
