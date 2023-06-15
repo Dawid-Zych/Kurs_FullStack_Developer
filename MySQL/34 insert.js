42c9531861f2a840a2f6a9b437303be3e21c32e2
Dawid Zych <dawid.aliens1989@gmail.com>
2023-06-15 16:19:10 +0200

    package-json

--- MySQL/34 insert.js
+++ MySQL/34 insert.js
@@ -1,56 +1,40 @@
-
-import mysql from "mysql2/promise";
+import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "test"
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'test',
 });
-
 
 await connection.connect();
 
 async function getAll() {
-    const query = "SELECT * FROM users";
-    const [data] = await connection.query(query);
-    return data;
+	const query = 'SELECT * FROM users';
+	const [data] = await connection.query(query);
+	return data;
 }
 
 //const data = await getAll();
 //console.log("data:", data);
 
-
 async function insertOne(user) {
-    const query = `
+	const query = `
         INSERT INTO users (name, surname, age, bio, address)
         VALUES (?, ?, ?, ?, ?)
     `;
 
-    const [result] = await connection.query(
-        query,
-        [user.name, user.surname, user.age,
-         user.bio, user.address]
-    );
+	const [result] = await connection.query(query, [user.name, user.surname, user.age, user.bio, user.address]);
 
-    return { ...user, id: result.insertId };
+	return { ...user, id: result.insertId };
 }
 
 const userDb = await insertOne({
-    name: "Alina",
-    surname: "Kowalska",
-    age: 30,
-    bio: "Programistka",
-    address: "Wilcza 111, Wawa"
+	name: 'Alina',
+	surname: 'Kowalska',
+	age: 30,
+	bio: 'Programistka',
+	address: 'Wilcza 111, Wawa',
 });
 
 console.log(userDb);
-
-
-
-
-
-
-
-
-
