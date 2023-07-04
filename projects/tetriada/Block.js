a4547b5fa902ea4fa7f665c63679a592055e7809
Dawid Zych <dawid.aliens1989@gmail.com>
2023-07-04 13:56:57 +0200

    losowy pattern i pozycja bloku na ekranie

--- projects/tetriada/Block.js
+++ projects/tetriada/Block.js
@@ -40,4 +40,41 @@ export default class Block {
 		const randIndex = Math.floor(Math.random() * this.blockTypes.length);
 		return this.blockTypes[randIndex];
 	};
+
+	nextPattern = () => {
+		this.activePatternIndex++;
+		if (this.activePatternIndex >= this.activeBlock.variants.length) {
+			this.activePatternIndex = 0;
+		}
+		return this.getActivePattern();
+	};
+
+	previousPattern = () => {
+		this.activePatternIndex--;
+		if (this.activePatternIndex < 0) {
+			this.activePatternIndex = this.activeBlock.variants.length - 1;
+		}
+		return this.getActivePattern();
+	};
+
+	getActivePattern = () => {
+		return this.activeBlock.variants[this.activePatternIndex];
+	};
+
+	drawOnBoard = board => {
+		const blockPattern = this.getActivePattern();
+
+		blockPattern.forEach((row, rowIndex) => {
+			row.forEach((col, colIndex) => {
+				if (col) {
+					board.drawBoardSquare(
+                        rowIndex + this.y,
+                        colIndex + this.x,
+                        this.activeBlock.color
+                          
+                  );
+				}
+			});
+		});
+	};
 }
