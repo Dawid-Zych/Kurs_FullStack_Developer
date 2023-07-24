25bbe2c1244fe4430aca36b84f3319a514a3d849
Dawid Zych <dawid.aliens1989@gmail.com>
2023-07-24 12:21:49 +0200

    fabryka dronow- model dron

--- Sequelize - ORM/41 drones - project.js
+++ Sequelize - ORM/41 drones - project.js
@@ -93,3 +93,51 @@ Address.hasOne(DronesFactory, {
 DronesFactory.belongsTo(Address, {
 	foreignKey: 'fk_address_id', // jest tylko w DronesFactory!
 });
+
+/* fabryka będzie produkować drony dlatego potrzebujemy kolejny model */
+
+const Drone = sequelize.define('Drone', {
+	vin: {
+		type: Sequelize.UUID,
+		defaultValue: Sequelize.UUIDV4,
+		primaryKey: true,
+	},
+	brand: {
+		type: DataTypes.STRING(24),
+		allowNull: true,
+		validate: { len: [1, 24] },
+	},
+	model: {
+		type: DataTypes.STRING(24),
+		allowNull: true,
+		validate: { len: [1, 24] },
+	},
+	price: {
+		type: DataTypes.DECIMAL(10, 2),
+		defaultValue: 0,
+		allowNull: true,
+		validate: {
+			isDecimal: true,
+			priceAboveZero(value) {
+				if (value < 0) throw new Error('Price cant be negative');
+			},
+		},
+	},
+	topSpeed: {
+		type: DataTypes.DECIMAL(5, 2),
+		defaultValue: 10.0,
+		allowNull: true,
+		validate: {
+			isDecimal: true,
+			min: 0.0,
+			max: 100.0,
+		},
+	},
+});
+
+DronesFactory.hasMany(Drone, {
+	foreignKey: 'fk_factory_id',
+});
+Drone.belongsTo(DronesFactory, {
+	foreignKey: 'fk_factory_id',
+});
