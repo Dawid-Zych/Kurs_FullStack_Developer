7ef08236b127d0a67e9e9a837cc897f025c9f8bd
Dawid Zych <dawid.aliens1989@gmail.com>
2023-08-01 18:23:43 +0200

    middleware sprawdzajacy dostep usera z rola

--- AUTH/monoose_acl/utility/aclauth.js
+++ AUTH/monoose_acl/utility/aclauth.js
@@ -1,7 +1,43 @@
 import { permissions } from './permissions.js';
 
+function getGuestDefaultUser() {
+	return {
+		role: 'user',
+	};
+}
+
 function authRole(req, res, next) {
-	return next();
+	/* 
+    req.passport.session: { user: 'asdadggadagw436qq'} albo undefined
+    req.user: {
+        _id: asdadasd,
+        password: 'sofvlfsdknhf;lqk34lk125',
+        email: 'ola@example.com,
+        role: 'user',
+        created: '',
+    }
+*/
+	console.log('authRole() - middlewere');
+	const resource = req.route.path; // /dashboard
+	const method = req.method.toLowerCase(); // get, post, etc
+	console.log('resource:', resource, 'method:', method);
+
+	if (!req.user) {
+		// jeśli nie  jest zalogowany to passport nie wstawid danych usera i nie ma role, tworzymy quest
+		req.user = getGuestDefaultUser();
+		// return res.redirect('/?msg=forbidden-access)
+	}
+
+	console.log('req.user', req.user);
+
+	if (permissions.isResourceAllowedForUser(req.user.role, resource, method)) {
+		// ma dostęp
+		return next();
+	} else {
+		// nie ma dostępu
+		res.status(401);
+		return res.send('Access forbidden');
+	}
 }
 
 export { authRole };
