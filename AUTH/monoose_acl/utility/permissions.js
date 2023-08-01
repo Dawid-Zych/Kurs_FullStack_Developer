e790894cf8b4f9162780eb2e42cc207b0926c826
Dawid Zych <dawid.aliens1989@gmail.com>
2023-08-01 18:01:18 +0200

    kopiowanie rol do roznych userow

--- AUTH/monoose_acl/utility/permissions.js
+++ AUTH/monoose_acl/utility/permissions.js
@@ -18,6 +18,16 @@ const usersRoles = [
 	},
 ];
 
-const permissions = {};
+const permissions = {
+	usersRoles: usersRoles,
+	addRoleParents: function (targetRole, sourceRole) {
+		const targetData = this.usersRoles.find(v => v.role === targetRole); //np obiekt z role admin
+		const sourceData = this.usersRoles.find(v => v.role === sourceRole); //np obiekt z role user
 
+		targetData.allows = targetData.allows.concat(sourceData.allows);
+	},
+};
+
+permissions.addRoleParents('admin', 'user');
+console.log(JSON.stringify(permissions.usersRoles, null, 4));
 export { permissions };
