81307b8fc6633e8a39c4b975ce7e4be43b4ed5e0
Dawid Zych <dawid.aliens1989@gmail.com>
2023-08-01 18:13:59 +0200

    sprawdzanie dostepnosci na serwerze ze wzgledu na role

--- AUTH/monoose_acl/utility/permissions.js
+++ AUTH/monoose_acl/utility/permissions.js
@@ -26,8 +26,41 @@ const permissions = {
 
 		targetData.allows = targetData.allows.concat(sourceData.allows);
 	},
+	isResourceAllowedForUser: function (usersRole, resource, method) {
+		const roleData = this.usersRoles.find(v => v.role === usersRole);
+		if (!roleData) return false; // brak dostępu bo nie ma takiej roli obsługiwanej na serwerze
+
+		const resourceData = roleData.allows.find(v => v.resource === resource);
+		if (!resourceData) return false; // osoba o tej roli nie ma info o tym adresie wiec nie ma dostępu
+		if (!resourceData.permissions) return false; // nie ma dostępu bo nie ma opisanych dozwolonych metod
+
+		if (!Array.isArray(resourceData.permissions)) {
+			if (resourceData.permissions === '*') return true; // dostęp do wszystkich metod
+			if (resourceData.permissions === method) return true; // ma dostęp do tej metody więc może korzystać
+		} else {
+			// tablica
+			if (resourceData.permissions.find(v => v === '8')) return true; // ma dosęp
+			if (resourceData.permissions.find(v => v === method)) return true; // ma dosęp
+		}
+
+		return false; // brak dostępu
+	},
 };
 
 permissions.addRoleParents('admin', 'user');
 console.log(JSON.stringify(permissions.usersRoles, null, 4));
+
+
+//testy
+console.log(permissions.isResourceAllowedForUser('admin', '/dashboard', 'get'));    // true
+console.log(permissions.isResourceAllowedForUser('admin', '/dashboard', 'delete')); //false
+console.log(permissions.isResourceAllowedForUser('admin', '/admin/users', 'get')); //true
+console.log(permissions.isResourceAllowedForUser('admin', '/api/data/10', 'get')); //false
+console.log(permissions.isResourceAllowedForUser('user', '/admin/users', 'get')); //false
+console.log(permissions.isResourceAllowedForUser('user', '/dashboard', 'get')); //true
+console.log(permissions.isResourceAllowedForUser('user', '/api/user/1', 'get')); //false
+console.log(permissions.isResourceAllowedForUser('quest', '/dashboard', 'get')); //false
+
+
+
 export { permissions };
