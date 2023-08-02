d295c5301c3df2ba4199c1b0fd95398d035de878
Dawid Zych <dawid.aliens1989@gmail.com>
2023-08-02 14:23:19 +0200

    model szkoly

--- AUTH/sequelize_acl/utility/db.js
+++ AUTH/sequelize_acl/utility/db.js
@@ -2,18 +2,17 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize('schoolacl', 'root', '', {
 	host: 'localhost',
-	dialect: 'mysql2',
+	dialect: 'mysql',
 	decimalNumbers: true,
 });
 
 sequelize
 	.authenticate()
 	.then(() => {
-		console.log('Connection has been established tothe database');
+		console.log('Connection has been established to the database');
 	})
 	.catch(err => {
 		console.log(err);
 	});
 
-	
 export { sequelize };
