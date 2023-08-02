23222a459a027136fa9685e7e58dfa8744fe07b7
Dawid Zych <dawid.aliens1989@gmail.com>
2023-08-02 14:53:40 +0200

    model przedmiotu

---
+++ AUTH/sequelize_acl/models/subject.model.js
@@ -0,0 +1,26 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../utility/db.js';
+
+const Subject = sequelize.define('Subject', {
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+		validate: {
+			isInt: true,
+		},
+	},
+	name: {
+		type: DataTypes.STRING(32),
+		allowNull: false,
+		validate: {
+			len: [1, 32],
+		},
+	},
+	description: {
+		type: DataTypes.TEXT,
+		allowNull: true,
+	},
+});
+
+export { Subject };
